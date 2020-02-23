e2f58ab4bca4dd377b4150d87f95511ca8208470
Pranav Khatri <Pranav8637@gmail.com>
2020-02-23 11:14:42 +0545

    I have added login and resigtration page and used a templete which I am still working on.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function notifications()
+    {
+        return view('admin.notifications');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route ::get('/','IndexController@index');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/notifications', 'HomeController@notifications')->name('notifications');
+Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
