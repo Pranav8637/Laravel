b8585da77e6e867798097a0996c7cfeb8acbddf2
Pranav Khatri <Pranav8637@gmail.com>
2020-04-01 23:06:57 +0545

    Change the product page design and content.

--- resources/views/productdetail.blade.php
+++ resources/views/productdetail.blade.php
@@ -3,62 +3,82 @@
 <!-- Styles -->
 <link href="{{asset('css/frontend_css/bootstrap.min.css')}}" rel="stylesheet">
 <link href="{{asset('css/frontend_css/owl.carousel.css')}}" rel="stylesheet">
-<link href="{{asset('css/frontend_css/stylesfrominternet.css')}}" rel="stylesheet">
+{{--<link href="{{asset('css/frontend_css/stylesfrominternet.css')}}" rel="stylesheet">--}}
 
 @section('content')
-    <div class="section-padding">
-        <div class="container">
-            <div class="product-information row">
-                <div class="col-lg-8 col-md-12 col-xs-12">
-{{--                    //app image--}}
-                    <div class="ads-details-wrapper">
-                        <div  class="owl-carousel owl-theme owl-loaded owl-drag" id="owl-demo">
-                            <!---->
-                            <div class="owl-stage-outer">
-                                <div class="owl-stage" style="transform: translate3d(-1280px, 0px, 0px); transition: all 0s ease 0s; width: 3200px;">
-                                    <div class="owl-item cloned" style="width: 610px; margin-right: 30px;">
-                                        <div  class="item">
-                                            <div  class="product-img d-flex justify-content-center"><img  alt="" class="img-fluid" src="https://choicemandu.com/image/cache/catalog/samsung-galaxy-a10s-1-1000x1000.jpg"><span  class="homes-tag featured pointer">Socheko</span><span  class="price-save">Rs. 12,999.00</span>
-                                                <!----><span  class="homes-tag rent"><i  class="lni-tag"></i> 16.1 % Off</span></div>
-                                        </div>
-                                    </div>
-                                    <div class="owl-item cloned" style="width: 610px; margin-right: 30px;">
-                                        <div  class="item">
-                                            <div  class="product-img d-flex justify-content-center"><img  alt="" class="img-fluid" src="https://choicemandu.com/image/cache/catalog/samsung-galaxy-a10s-1-1000x1000.jpg"><span  class="homes-tag featured pointer">Socheko</span><span  class="price-save">Rs. 12,999.00</span>
-                                                <!----><span  class="homes-tag rent"><i  class="lni-tag"></i> 16.1 % Off</span></div>
-                                        </div>
-                                    </div>
-                                    <div class="owl-item active" style="width: 610px; margin-right: 30px;">
-                                        <div  class="item">
-                                            <div  class="product-img d-flex justify-content-center"><img  alt="" class="img-fluid" src="https://choicemandu.com/image/cache/catalog/samsung-galaxy-a10s-1-1000x1000.jpg"><span  class="homes-tag featured pointer">Socheko</span><span  class="price-save">Rs. 12,999.00</span>
-                                                <!----><span  class="homes-tag rent"><i  class="lni-tag"></i> 16.1 % Off</span></div>
-                                        </div>
-                                    </div>
-                                    <div class="owl-item cloned" style="width: 610px; margin-right: 30px;">
-                                        <div  class="item">
-                                            <div  class="product-img d-flex justify-content-center"><img  alt="" class="img-fluid" src="https://choicemandu.com/image/cache/catalog/samsung-galaxy-a10s-1-1000x1000.jpg"><span  class="homes-tag featured pointer">Socheko</span><span  class="price-save">Rs. 12,999.00</span>
-                                                <!----><span  class="homes-tag rent"><i  class="lni-tag"></i> 16.1 % Off</span></div>
-                                        </div>
-                                    </div>
-                                    <div class="owl-item cloned" style="width: 610px; margin-right: 30px;">
-                                        <div  class="item">
-                                            <div  class="product-img d-flex justify-content-center"><img  alt="" class="img-fluid" src="https://choicemandu.com/image/cache/catalog/samsung-galaxy-a10s-1-1000x1000.jpg"><span  class="homes-tag featured pointer">Socheko</span><span  class="price-save">Rs. 12,999.00</span>
-                                                <!----><span  class="homes-tag rent"><i  class="lni-tag"></i> 16.1 % Off</span></div>
-                                        </div>
-                                    </div>
-                                </div>
+    <div class="body-wrpr clearfix">
+        <div class="prdct-dtl__ttl-cntnr">
+            <h1 itemprop="name" class="prdct-dtl__ttl" data-mspid="12867">Samsung Galaxy S9 </h1><span class="prdct-dtl__ttl-vrnt" data-size="64 GB">(64 GB)</span>
+            <div class="prdct-dtl__tlbr clearfix">
+                <div class="prdct-dtl__tlbr-rtng"> <span class="prdct-dtl__tlbr-item tlbr-exprt js-inpg-link" data-href="expert-reviews"> <span class="prdct-dtl__tlbr-item__scre">8.2</span> Expert Score </span> <span class="prdct-dtl__tlbr-item tlbr-rtngs js-tltp js-inpg-link" data-tooltip="Rated 4.4/5 by 4858 users" title="Rated 4.4/5 by 4858 users" data-href="user-reviews"> <span class="prdct-dtl__tlbr-item__scre" itemprop="ratingValue">4.4</span> <span itemprop="ratingCount">4858</span> Ratings </span> <a rel="nofollow" class="prdct-dtl__tlbr-item" target="_blank" href="https://www.samsung.com/in/smartphones/galaxy-s9/">Official Website</a> <span class="prdct-dtl__tlbr-item"> <span> <div class="js-open-link" data-open-link="https://hi.mysmartprice.com/mobile/samsung-galaxy-s9-msp12867"> हिंदी में पढ़ें </div> </span> </span>
+                </div>
+            </div>
+        </div>
+        <div class="sctn prdct-dtl clearfix">
+            <div class="prdct-dtl__left">
+                <div class="prdct-dtl__img-wrpr"> <img class="prdct-dtl__img" src="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-1.jpg" data-image="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-1.jpg" data-thumb-id="1" data-media_position="1" data-media_key="inventoryImages" alt="Samsung Galaxy S9" title="Samsung Galaxy S9"> </div>
+                <div class="prdct-dtl__thmbnl-wrpr " data-video-id="IN9_tpyFqsM">
+                    <div class="prdct-dtl__thmbnl" data-href="multiple_media_popup.php"> <img class="prdct-dtl__thmbnl-img" src="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-1.jpg" data-image="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-1.jpg" alt="Samsung Galaxy S9" data-thumb-id="1" data-media_key="inventoryImages" data-media_position="1"> </div>
+                    <div class="prdct-dtl__thmbnl" data-href="multiple_media_popup.php"> <img class="prdct-dtl__thmbnl-img" src="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-2.jpg" data-image="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-2.jpg" alt="Samsung Galaxy S9" data-thumb-id="2" data-media_position="2" data-media_key="inventoryImages"> </div>
+                    <div class="prdct-dtl__thmbnl" data-href="multiple_media_popup.php"> <img class="prdct-dtl__thmbnl-img" src="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-3.jpg" data-image="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-3.jpg" alt="Samsung Galaxy S9" data-thumb-id="3" data-media_position="3" data-media_key="inventoryImages"> </div>
+                    <div class="prdct-dtl__thmbnl" data-href="multiple_media_popup.php"> <img class="prdct-dtl__thmbnl-img" src="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-4.jpg" data-image="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-4.jpg" alt="Samsung Galaxy S9" data-thumb-id="4" data-media_position="4" data-media_key="inventoryImages"> </div>
+                    <div class="prdct-dtl__thmbnl" data-href="multiple_media_popup.php"> <img class="prdct-dtl__thmbnl-img" src="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-5.jpg" data-image="https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/12867-82-5.jpg" alt="Samsung Galaxy S9" data-thumb-id="5" data-media_position="5" data-media_key="inventoryImages"> </div>
+                    <div class="prdct-dtl__thmbnl prdct-dtl__thmbnl--vdo js-tltp prdct-dtl__thmbnl--slctd" data-href="multiple_media_popup.php" data-tooltip="Click to play video"> <img class="prdct-dtl__thmbnl-img" src="https://img.youtube.com/vi/IN9_tpyFqsM/0.jpg" data-image="https://img.youtube.com/vi/IN9_tpyFqsM/0.jpg" alt="Samsung Galaxy S9" data-thumb-id="6" data-media_position="1" data-media_key="customerReviewVideos"> <span class="prdct-dtl__thmbnl__wtch">Video</span> </div>
+                </div>
+            </div>
+            <div class="prdct-dtl__rght">
+                <div class="prdct-dtl__cntr">
+                    <div class="prdct-dtl__prc"> <span class="prdct-dtl__prc-lbl">Best Price:</span> <span>₹</span> <span class="prdct-dtl__prc-val">28,499</span> </div>
+                    <div class="prdct-dtl__vrnt-wrpr">
+                        <div class="prdct-dtl__vrnt-item prdct-dtl__vrnt-size">
+                            <div class="prdct-dtl__vrnt-hdr clearfix">
+                                <div class="prdct-dtl__vrnt-ttl">VARIANTS</div>
                             </div>
-                            <div class="owl-nav disabled">
-                                <div class="owl-prev"><i class="lni-chevron-left"></i></div>
-                                <div class="owl-next"><i class="lni-chevron-right"></i></div>
-                            </div>
-                            <div class="owl-dots disabled">
-                                <div class="owl-dot active"><span></span></div>
+                            <div class="avlbl-sizes">
+                                <div class="avlbl-sizes__item-wrpr js-open-link avlbl-sizes__item-wrpr--slctd " data-open-link="https://www.mysmartprice.com/mobile/samsung-galaxy-s9-msp12867"> <span class="avlbl-sizes__item">64 GB</span> </div>
+                                <div class="avlbl-sizes__item-wrpr js-open-link " data-open-link="https://www.mysmartprice.com/mobile/samsung-galaxy-s9-128gb-msp14577"> <span class="avlbl-sizes__item">128 GB</span> </div>
+                                <div class="avlbl-sizes__item-wrpr js-open-link " data-open-link="https://www.mysmartprice.com/mobile/samsung-galaxy-s9-256gb-msp14299"> <span class="avlbl-sizes__item">256 GB</span> </div>
                             </div>
                         </div>
                     </div>
+                    <div class="prc-box prc-tbl--fullcards">
+                        <div class="prc-tbl__row-wrpr " data-storename="flipkart">
+                            <div class="prc-tbl__row clearfix">
+                                <div class="prc-tbl__logo"><img class="prdct-dtl__str-icon" src="https://assets.mspimages.in/logos/store-logo/flipkart_store.png"></div>
+                                <div class="prc-tbl__info">
+                                    <div class="prc-tbl__prc">₹28,499</div>
+                                </div>
+                                <div class="prc-tbl__btn">
+                                    <a rel="nofollow" target="_blank" href="https://www.mysmartprice.com/out/sendtostore.php?mspid=12867&amp;access_point=desktop&amp;l1=c&amp;top_category=electronics&amp;category=mobile&amp;id=301651430&amp;store=flipkart&amp;gtsprice=28499&amp;hashmap=aHR0cHM6Ly93d3cuZmxpcGthcnQuY29tL3NhbXN1bmctZ2FsYXh5LXM5LW1pZG5pZ2h0LWJsYWNrLTY0LWdiL3AvaXRtZjMzYTY5cnBzemd6bj9waWQ9TU9CRjJWV1ZCR0NUNVFRTg==" class="bttn bttn--gts prc-tbl__gts-btn js-prc-tbl__gts-btn" data-url="https://www.mysmartprice.com/out/sendtostore.php?mspid=12867&amp;access_point=desktop&amp;l1=c&amp;top_category=electronics&amp;category=mobile&amp;id=301651430&amp;store=flipkart&amp;gtsprice=28499&amp;hashmap=aHR0cHM6Ly93d3cuZmxpcGthcnQuY29tL3NhbXN1bmctZ2FsYXh5LXM5LW1pZG5pZ2h0LWJsYWNrLTY0LWdiL3AvaXRtZjMzYTY5cnBzemd6bj9waWQ9TU9CRjJWV1ZCR0NUNVFRTg=="></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="prc-tbl__row-wrpr " data-storename="amazon">
+                            <div class="prc-tbl__row clearfix">
+                                <div class="prc-tbl__logo"><img class="prdct-dtl__str-icon" src="https://assets.mspimages.in/logos/store-logo/amazon_store.png"></div>
+                                <div class="prc-tbl__info">
+                                    <div class="prc-tbl__prc">₹29,700</div>
+                                </div>
+                                <div class="prc-tbl__btn">
+                                    <a rel="nofollow" target="_blank" href="https://www.mysmartprice.com/out/sendtostore.php?mspid=12867&amp;access_point=desktop&amp;l1=c&amp;top_category=electronics&amp;category=mobile&amp;id=302472573&amp;store=amazon&amp;gtsprice=29700&amp;hashmap=aHR0cDovL3d3dy5hbWF6b24uaW4vZ3Avb2ZmZXItbGlzdGluZy9CMDdDOVk4VFNXLz9zb3J0PXByaWNl" class="bttn bttn--gts prc-tbl__gts-btn js-prc-tbl__gts-btn" data-url="https://www.mysmartprice.com/out/sendtostore.php?mspid=12867&amp;access_point=desktop&amp;l1=c&amp;top_category=electronics&amp;category=mobile&amp;id=302472573&amp;store=amazon&amp;gtsprice=29700&amp;hashmap=aHR0cDovL3d3dy5hbWF6b24uaW4vZ3Avb2ZmZXItbGlzdGluZy9CMDdDOVk4VFNXLz9zb3J0PXByaWNl"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="kyspc clearfix">
+                        <h3 class="kyspc__hdr"> Key Specifications  </h3>
+                        <ul class="kyspc__list clearfix">
+                            <li class="kyspc__item kyspc__item--cpu">Samsung Exynos 9810</li>
+                            <li class="kyspc__item kyspc__item--ram">4GB RAM</li>
+                            <li class="kyspc__item kyspc__item--strge">64GB internal storage</li>
+                            <li class="kyspc__item kyspc__item--bttry">3000 mAh battery</li>
+                            <li class="kyspc__item kyspc__item--cmra">12MP rear &amp; 8MP selfie</li>
+                            <li class="kyspc__item kyspc__item--aspct">5.8 inches</li>
+                            <li class="kyspc__item kyspc__item--sim">Dual, Nano-SIM (Hybrid Slot )</li>
+                            <li class="kyspc__item kyspc__item--os">Android v8.0 (Oreo)</li>
+                        </ul>
+                    </div>
                 </div>
-
             </div>
         </div>
     </div>
